1855e3562d4e4b0ad1d2f22e86e0e890d0dadb4c
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-10-07 13:12:34 +0300

    implemented functionality for allSubset of a list

--- Examples/src/Main.java
+++ Examples/src/Main.java
@@ -2,14 +2,20 @@ import utils.math.MathUtil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
         ArrayList<ArrayList<Integer>> permutedList;
-        permutedList = MathUtil.permuteList(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4)));
+        permutedList = MathUtil.permuteList(new ArrayList<>(Arrays.asList(1, 2, 4)));
+
+
+        Set<Set<Integer>> allSubSets = MathUtil.getAllSubsetsOfSet(new ArrayList<>(Arrays.asList(1, 2, 4)));
 
         System.out.println(permutedList);
+        System.out.println();
+        System.out.println(allSubSets);
 
     }
 }
--- Examples/src/utils/math/MathUtil.java
+++ Examples/src/utils/math/MathUtil.java
@@ -4,6 +4,8 @@ import utils.array.ArrayUtil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static define.Predef.requier;
 
@@ -11,13 +13,13 @@ public class MathUtil {
 
     public static <T> ArrayList<ArrayList<T>> permuteList(ArrayList<T> list) {
 
+        requier(list != null && list.size() >= 2);
         int listSize = list.size();
-        requier(listSize >= 2);
         ArrayList<ArrayList<T>> permutedLists = new ArrayList<>(listSize);
 
         if (listSize == 2) {
             permutedLists.add(list);
-            permutedLists.add(new ArrayList<T>(Arrays.asList(list.get(1), list.get(0))));
+            permutedLists.add(new ArrayList<>(Arrays.asList(list.get(1), list.get(0))));
             return permutedLists;
         } else {
             for (int i = 0; i < listSize; i++) {
@@ -29,6 +31,26 @@ public class MathUtil {
                 }
             }
             return permutedLists;
+        }
+    }
+
+    public static <T> Set<Set<T>> getAllSubsetsOfSet(ArrayList<T> list) {
+
+        requier(list != null && list.size() >= 1);
+        Set<Set<T>> allSubsets = new HashSet<>();
+        if (list.size() == 1) {
+            allSubsets.add(new HashSet<T>());
+            allSubsets.add(new HashSet<>(list));
+            return allSubsets;
+        } else {
+            T elem = list.remove(list.size() - 1);
+            Set<Set<T>> subsets = getAllSubsetsOfSet(list);
+            for (Set<T> t : subsets) {
+                allSubsets.add(new HashSet<>(t));
+                t.add(elem);
+                allSubsets.add(new HashSet<>(t));
+            }
+            return allSubsets;
         }
     }
 }
