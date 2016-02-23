6514de8811d15466027c41ba884fa5e39f607193
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2016-02-23 16:14:09 +0200

    Java Sort

---
+++ Examples/src/_hackerrank/collections/JavaSort.java
@@ -0,0 +1,68 @@
+package _hackerrank.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class JavaSort {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<>();
+        while (testCases > 0) {
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+        Collections.sort(studentList);
+        for (Student st : studentList) {
+            System.out.println(st.getFname());
+        }
+    }
+}
+
+class Student implements Comparable<Student> {
+    private int id;
+    private String fname;
+    private double cgpa;
+
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    @Override
+    public int compareTo(Student that) {
+        if (this.getCgpa() == that.getCgpa()) {
+            if(this.getFname() == that.getFname()){
+                return Integer.compare(that.getId(), this.getId());
+            }else{
+                return this.getFname().compareTo(that.getFname());
+            }
+        } else {
+            return Double.compare(that.getCgpa(), this.getCgpa());
+        }
+    }
+}
+
