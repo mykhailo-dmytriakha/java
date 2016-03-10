5983dd7c0eb15ad9911347ea7a15e5b6e9e23dee
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2016-03-10 11:25:07 +0200

    Java Dequeue

--- Examples/src/_hackerrank/collections/JavaDequeue.java
+++ Examples/src/_hackerrank/collections/JavaDequeue.java
@@ -1,35 +1,58 @@
 package _hackerrank.collections;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * task description
  * <b>https://www.hackerrank.com/challenges/java-dequeue/submissions/code/17812480</b>
  */
 public class JavaDequeue {
+    private static Deque<Integer> numbers;
+    private static HashMap<Integer, Integer> uniqueNumbers;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int max = 0;
         int n = in.nextInt();
         int m = in.nextInt();
-        Deque<Integer> numbers = new ArrayDeque<>(n);
-        Map<Integer, Integer> uniqueNumbers = new HashMap<>(m);
+        numbers = new ArrayDeque<>(n);
+        uniqueNumbers = new HashMap<>(n);
 
         for (int i = 0; i < n; i++) {
             int num = in.nextInt();
-            numbers.add(num);
-            if(numbers.size() == m){
-                Integer uniqueNumber = getAmountOfUniqueNumbers(numbers);
-                if(uniqueNumber >max) {
+            addElement(num);
+            if (numbers.size() == m) {
+                Integer uniqueNumber = getAmountOfUniqueNumbers();
+                if (uniqueNumber > max) {
                     max = uniqueNumber;
                 }
-                numbers.pollFirst();
+                removeElement();
             }
         }
         System.out.println(max);
     }
 
-    private static Integer getAmountOfUniqueNumbers(Deque<Integer> subArray) {
-        return new HashMap<Integer, Integer>().size();
+    private static void removeElement() {
+        Integer num = numbers.removeFirst();
+        Integer value = uniqueNumbers.get(num);
+        if (value == 1) {
+            uniqueNumbers.remove(num);
+        } else {
+            uniqueNumbers.put(num, --value);
+        }
+    }
+
+    private static void addElement(int num) {
+        numbers.addLast(num);
+
+        Integer value = uniqueNumbers.get(num);
+        uniqueNumbers.put(num, (null == value) ? 1 : ++value);
+    }
+
+    private static Integer getAmountOfUniqueNumbers() {
+        return uniqueNumbers.size();
     }
 }
