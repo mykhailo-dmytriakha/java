bb324e8dede806ccd4e378ac1371b4ca94d6339d
MichaelDm <dmmikl86@gmail.com>
2016-06-07 01:17:05 +0300

    working on panagrams

---
+++ Examples/src/_hackerrank/algorithms/strings/Pangrams.java
@@ -0,0 +1,19 @@
+package _hackerrank.algorithms.strings;
+
+import java.util.Scanner;
+
+public class Pangrams {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        if(isPangram(line.toLowerCase().trim())){
+            System.out.println("pangram");
+        }else {
+            System.out.println("not pangram");
+        }
+    }
+
+    private static boolean isPangram(String line) {
+        return setOfChars.equals(allAlphabetChars);
+    }
+}
