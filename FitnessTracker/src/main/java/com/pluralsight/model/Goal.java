96b5091e7dd0eada79761c35fae8205b290a524d
mdmytriaha <mdmytriaha@youa.vn.ua>
2016-10-17 10:37:52 +0300

    springJpa: complete module 6: Overview of JPA and creating your first entity

--- FitnessTracker/src/main/java/com/pluralsight/model/Goal.java
+++ FitnessTracker/src/main/java/com/pluralsight/model/Goal.java
@@ -2,7 +2,16 @@ package com.pluralsight.model;
 
 import org.hibernate.validator.constraints.Range;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Goal {
+
+	@Id
+	@GeneratedValue
+	private Long id;
 
 	@Range(min = 1, max = 120)
 	private int minutes;
@@ -13,5 +22,13 @@ public class Goal {
 
 	public void setMinutes(int minutes) {
 		this.minutes = minutes;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 }
